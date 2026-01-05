ab8dea0f3dd85f33e90b568836ea0f03519d3dfd
garvitk212-ops <garvitk212@gmail.com>
2026-01-05 14:38:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hatsune Miku is online 💙");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Web server running on port ${PORT}`);
+});
 const {
   Client,
   GatewayIntentBits,
